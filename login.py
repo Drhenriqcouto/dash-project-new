e2063b8aa1a83ea3877b0eb73393078cf36de0fc
Henrique Toledo <henriqcouto@gmail.com>
2024-09-17 00:20:10 -0300

    Change file

--- login.py
+++ login.py
@@ -1,21 +1,28 @@
-from pandas_datareader import data as web
+import streamlit as st
 import pandas as pd
 import yfinance as yf
-import numpy as np
 import matplotlib.pyplot as plt
-from openpyxl import Workbook
-from openpyxl.utils.dataframe import dataframe_to_rows
-from datetime import datetime, timedelta
-import streamlit as st
-import os
-import csv
-from A1 import executar_operacao
+from datetime import datetime
 
+# Função para autenticar com base no arquivo Excel
+def authenticate(username, password):
+    try:
+        # Carregar o arquivo de registros
+        registros_df = pd.read_excel('registros.xlsx')
 
+        # Verificar se o usuário e senha estão na planilha
+        usuario_validado = registros_df[
+            (registros_df['username'] == username) & (registros_df['password'] == password)
+        ]
+        return not usuario_validado.empty
+    except Exception as e:
+        st.error(f"Erro ao carregar o arquivo de registros: {e}")
+        return False
 
-
-# Carregando os resultados
-df_resultados = pd.read_excel('resultados_trading.xlsx')
+# Função para exibir o relatório
+def show_report():
+    st.title("Relatório HTrading")
+    st.write("Aqui está o relatório da API HTrading.")
 
 # Função para exibir o relatório dos melhores resultados
 def exibir_relatorio(df):
@@ -46,8 +53,8 @@ def exibir_relatorio(df):
                 ### Ativo: **{row['Nome do ativo']}**
                 
                 - **Índice Sharpe:** {row['Índice Sharpe']:.2f}
-                - **Ganho (%):** {percentual_acerto:.2f}%
-                - **Perda (%):** {percentual_erro:.2f}%
+                - **Ganho (%):** {percentual_acerto:.2f}% 
+                - **Perda (%):** {percentual_erro:.2f}% 
                 - **Tipo de operação:** {tipo_operacao}
                 - **Nível percentual de compra ou venda:** {nivel_percentual:.2f}%
                 
@@ -65,7 +72,6 @@ def calcular_preco_entrada(preco_atual, percentual, tipo_entrada):
     
     return preco_entrada
 
-
 # Função para acionar o código 2 (rastreio)
 def rastrear():
     listadas = pd.read_csv('listadas.csv')
@@ -76,21 +82,16 @@ def rastrear():
 
     data_atual = datetime.now().strftime('%Y-%m-%d')
     # Código 2 que faz o rastreamento dos ativos e cálculos
-    # Definindo o intervalo de valores para a variável valor
     valores = [0.01, 0.02, 0.03, 0.04, 0.05]
-    # Definindo os valores possíveis para a variável contador
     contadores = [0, 1, 2, -2]
     bet_size = 100
 
-    # Listas para armazenar os resultados
     resultados = []
 
-    # Itera sobre todos os ativos definidos em codigos
     for ativo in codigos:
         x = yf.Ticker(ativo)
         cotacoes = x.history(start='2022-01-01', end=data_atual)
 
-        # Cria o DataFrame com os dados de cotações
         df = pd.DataFrame()
         df.index = cotacoes.index
         df['Abertura'] = cotacoes['Open']
@@ -102,7 +103,6 @@ def rastrear():
         df['A1'] = cotacoes['Close'].shift(1) - cotacoes['Open'].shift(1)
         df['A2'] = cotacoes['Close'].shift(2) - cotacoes['Open'].shift(2)
 
-        # Calcula as variações
         Var_Fechamento = df['Fechamento'] / df['Fechamento'].shift(1) - 1
         Var_Abertura = df['Abertura'] / df['Fechamento'].shift(1) - 1
         Var_Maxima = df['Máxima'] / df['Fechamento'].shift(1) - 1
@@ -174,7 +174,6 @@ def rastrear():
                                 'Ajuste': row['A1']
                             })
 
-                # Cria o DataFrame com os trades e calcula o Sharpe Ratio
                 df_trades_raw = pd.DataFrame(listadetrades)
                 if not df_trades_raw.empty:
                     df_trades1 = df_trades_raw
@@ -210,129 +209,151 @@ def rastrear():
     df_resultados.to_excel('resultados_trading.xlsx', index=False)
     st.success("Rastreamento concluído. Resultados atualizados!")
 
-
-
-# Barra de navegação do menu
-menu = ["Home", "Relatório", "Monte a sua Operação", "Rastreador", "Análise"]
-opcao = st.sidebar.selectbox("Menu", menu)
-
-# Lógica do menu
-if opcao == "Home":
-    st.title("Bem-vindo ao HC Trading")
-    st.write(df_resultados)
-
-elif opcao == "Relatório":
-    st.title("Relatório de Resultados")
-    exibir_relatorio(df_resultados)
-
-elif opcao == "Monte a sua Operação":
-    st.title("Monte a sua Operação")
+# Página de Login
+def login_page():
+    st.title("Login")
+    username = st.text_input("Nome de Usuário")
+    password = st.text_input("Senha", type="password")
     
-    # Lista suspensa com os ativos disponíveis
-    ativo = st.selectbox("Selecione o Ativo", df_resultados['Nome do ativo'].unique())
-    
-    # Campo para preencher o preço atual
-    preco_atual = st.number_input("Preço Atual", min_value=0.0, format="%.2f")
-    
-    # Lista suspensa para selecionar o percentual
-    percentual = st.selectbox("Selecione o Percentual", [0.01, 0.02, 0.03, 0.04, 0.05])
-    
-    # Lista suspensa para selecionar o tipo de entrada
-    tipo_entrada = st.selectbox("Selecione o Tipo de Entrada", 
-                                ["Compra na baixa", "Venda na alta", "Compra na alta", "Venda na baixa"])
-    
-    # Botão para calcular o preço de entrada
-    if st.button("Calcular Preço"):
-        if preco_atual > 0:
-            preco_entrada = calcular_preco_entrada(preco_atual, percentual, tipo_entrada)
-            st.write(f"### Preço de Entrada Calculado: R$ {preco_entrada:.2f}")
+    if st.button("Entrar"):
+        if authenticate(username, password):
+            st.session_state.authenticated = True
+            st.experimental_rerun()
         else:
-            st.write("Por favor, insira um preço atual válido.")
+            st.error("Nome de usuário ou senha incorretos.")
 
-elif opcao == "Rastreador":
-    st.title("Última Análise")
-    st.dataframe(df_resultados)
+# Página Principal
+def main():
+    # Carregando os resultados
+    global df_resultados
+    df_resultados = pd.read_excel('resultados_trading.xlsx')
 
-    # Botão para realizar um novo rastreio
-    if st.button("Novo Rastreio"):
-        rastrear()
-        st.experimental_rerun()
+    # Barra de navegação do menu
+    menu = ["Home", "Relatório", "Monte a sua Operação", "Rastreador", "Análise"]
+    opcao = st.sidebar.selectbox("Menu", menu)
 
-elif opcao == "Análise":
-    # Interface do Streamlit
-    st.title("Monte a sua Análise")
+    # Lógica do menu
+    if opcao == "Home":
+        st.title("Bem-vindo ao HC Trading")
+        st.write(df_resultados)
 
-    # Carregar a lista de ativos
-    codigos = pd.read_excel('selecao.xlsx')
-    codigos['Simbol'] = codigos['codigo'] + '.SA'
+    elif opcao == "Relatório":
+        st.title("Relatório de Resultados")
+        exibir_relatorio(df_resultados)
 
-    # Campo para o usuário selecionar o ativo
-    ativo_selecionado = st.selectbox(
-        "Selecione o Ativo",
-        codigos['Simbol']
-    )
-
-    # Campo para o usuário digitar o valor percentual
-    valor = st.number_input("Digite o valor percentual (ex: 0.02 para 2%)", min_value=0.0, format="%.4f")
-
-    # Campo para o usuário selecionar o tipo de operação
-    tipo_operacao = st.selectbox(
-        "Selecione o Tipo de Operação",
-        ["Compra na baixa", "Venda na alta", "Venda na baixa", "Compra na alta"]
-    )
-   # Campo para o usuário inserir o período (número de dias)
-    periodo = st.number_input("Período (número de dias)", min_value=1, step=1)
-
-   
-    if st.button("Executar Análise"):
-        resultado = executar_operacao(valor, tipo_operacao, periodo, ativo_selecionado)
+    elif opcao == "Monte a sua Operação":
+        st.title("Monte a sua Operação")
         
-        # Exibir os resultados
-        st.write("Resultado da Análise:")
-        st.dataframe(resultado)
+        # Lista suspensa com os ativos disponíveis
+        ativo = st.selectbox("Selecione o Ativo", df_resultados['Nome do ativo'].unique())
+        
+        # Campo para preencher o preço atual
+        preco_atual = st.number_input("Preço Atual", min_value=0.0, format="%.2f")
+        
+        # Lista suspensa para selecionar o percentual
+        percentual = st.selectbox("Selecione o Percentual", [0.01, 0.02, 0.03, 0.04, 0.05])
+        
+        # Lista suspensa para selecionar o tipo de entrada
+        tipo_entrada = st.selectbox("Selecione o Tipo de Entrada", 
+                                    ["Compra na baixa", "Venda na alta", "Compra na alta", "Venda na baixa"])
+        
+        # Botão para calcular o preço de entrada
+        if st.button("Calcular Preço"):
+            if preco_atual > 0:
+                preco_entrada = calcular_preco_entrada(preco_atual, percentual, tipo_entrada)
+                st.write(f"### Preço de Entrada Calculado: R$ {preco_entrada:.2f}")
+            else:
+                st.write("Por favor, insira um preço atual válido.")
 
-        # Calcular e exibir o pior dia
-        pior_dia = round(resultado['Resultado'].min(), 2)
-        st.write(f"Pior dia: R$ {pior_dia}")
+    elif opcao == "Rastreador":
+        st.title("Última Análise")
+        st.dataframe(df_resultados)
 
-        # Calcular e exibir o melhor dia
-        melhor_dia = round(resultado['Resultado'].max(), 2)
-        st.write(f"Melhor dia: R$ {melhor_dia}")
+        # Botão para realizar um novo rastreio
+        if st.button("Novo Rastreio"):
+            rastrear()
+            st.experimental_rerun()
 
-        # Calcular e exibir o ganho médio
-        ganho_medio = pd.DataFrame()
-        ganho_medio['Ganho Médio'] = (resultado['Resultado'] / (resultado['price'] * 10))
-        valor = round(ganho_medio['Ganho Médio'].mean() * 100, 2)
-        st.write(f"Ganho médio: {valor}% ao dia")
+    elif opcao == "Análise":
+        # Interface do Streamlit
+        st.title("Monte a sua Análise")
 
-        # Percentual de Gain e Loss
-        x = ((resultado['Resultado'] > 0) == True).sum()
-        y = ((resultado['Resultado'] > 0) == False).sum()
-        gain = round((x / (x + y)) * 100, 2)
-        loss = round((y / (x + y)) * 100, 2)
-        st.write(f"Percentual de acerto: {gain}%")
-        st.write(f"Percentual de erro: {loss}%")
+        # Carregar a lista de ativos
+        codigos = pd.read_excel('selecao.xlsx')
+        codigos['Simbol'] = codigos['codigo'] + '.SA'
 
-        # Avaliação da estratégia
-        resultado['ret_acumulado'] = resultado['Resultado'].cumsum()
-        st.write("Gráfico de Retorno Acumulado:")
-        st.line_chart(resultado['ret_acumulado'])
+        # Campo para o usuário selecionar o ativo
+        ativo_selecionado = st.selectbox(
+            "Selecione o Ativo",
+            codigos['Simbol']
+        )
 
-        # DrawDown
-        capital_inicial = 1000
-        resultado['ret_acumulado'] += capital_inicial
-        lucro = round(resultado['Resultado'].sum(), 2)
-        drawdown = resultado['ret_acumulado'] - resultado['ret_acumulado'].cummax()
-        resultado['drawdown'] = drawdown
+        # Campo para o usuário digitar o valor percentual
+        valor = st.number_input("Digite o valor percentual (ex: 0.02 para 2%)", min_value=0.0, format="%.4f")
 
-        fig, ax = plt.subplots(2, 1, sharex=True, figsize=(20, 8))
-        ax[0].plot(resultado['ret_acumulado'])
-        ax[0].set_title("Retorno Acumulado")
-        ax[1].plot(resultado['drawdown'], color='red')
-        ax[1].set_title("Drawdown")
-        st.pyplot(fig)
+        # Campo para o usuário selecionar o tipo de operação
+        tipo_operacao = st.selectbox(
+            "Selecione o Tipo de Operação",
+            ["Compra na baixa", "Venda na alta", "Venda na baixa", "Compra na alta"]
+        )
+        # Campo para o usuário inserir o período (número de dias)
+        periodo = st.number_input("Período (número de dias)", min_value=1, step=1)
 
-        # Calculando o período de drawdown
-        resultado['underwater'] = resultado['ret_acumulado'] < resultado['ret_acumulado'].cummax()
-        periodo_drawdown = round(resultado['underwater'].sum() / 8, 2)
-        st.write(f"Período de drawdown: {periodo_drawdown}")
+        if st.button("Executar Análise"):
+            resultado = executar_operacao(valor, tipo_operacao, periodo, ativo_selecionado)
+            
+            # Exibir os resultados
+            st.write("Resultado da Análise:")
+            st.dataframe(resultado)
+
+            # Calcular e exibir o pior dia
+            pior_dia = round(resultado['Resultado'].min(), 2)
+            st.write(f"Pior dia: R$ {pior_dia}")
+
+            # Calcular e exibir o melhor dia
+            melhor_dia = round(resultado['Resultado'].max(), 2)
+            st.write(f"Melhor dia: R$ {melhor_dia}")
+
+            # Calcular e exibir o ganho médio
+            ganho_medio = pd.DataFrame()
+            ganho_medio['Ganho Médio'] = (resultado['Resultado'] / (resultado['price'] * 10))
+            valor = round(ganho_medio['Ganho Médio'].mean() * 100, 2)
+            st.write(f"Ganho médio: {valor}% ao dia")
+
+            # Percentual de Gain e Loss
+            x = ((resultado['Resultado'] > 0) == True).sum()
+            y = ((resultado['Resultado'] > 0) == False).sum()
+            gain = round((x / (x + y)) * 100, 2)
+            loss = round((y / (x + y)) * 100, 2)
+            st.write(f"Percentual de acerto: {gain}%")
+            st.write(f"Percentual de erro: {loss}%")
+
+            # Avaliação da estratégia
+            resultado['ret_acumulado'] = resultado['Resultado'].cumsum()
+            st.write("Gráfico de Retorno Acumulado:")
+            st.line_chart(resultado['ret_acumulado'])
+
+            # DrawDown
+            capital_inicial = 1000
+            resultado['ret_acumulado'] += capital_inicial
+            lucro = round(resultado['Resultado'].sum(), 2)
+            drawdown = resultado['ret_acumulado'] - resultado['ret_acumulado'].cummax()
+            resultado['drawdown'] = drawdown
+
+            fig, ax = plt.subplots(2, 1, sharex=True, figsize=(20, 8))
+            ax[0].plot(resultado['ret_acumulado'])
+            ax[0].set_title("Retorno Acumulado")
+            ax[1].plot(resultado['drawdown'], color='red')
+            ax[1].set_title("Drawdown")
+            st.pyplot(fig)
+
+            # Calculando o período de drawdown
+            resultado['underwater'] = resultado['ret_acumulado'] < resultado['ret_acumulado'].cummax()
+            periodo_drawdown = round(resultado['underwater'].sum() / 8, 2)
+            st.write(f"Período de drawdown: {periodo_drawdown}")
+
+# Verifica se o usuário está autenticado
+if 'authenticated' not in st.session_state or not st.session_state.authenticated:
+    login_page()
+else:
+    main()
